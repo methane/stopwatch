6c2561a04fcc7958f6d75fac33170c4959f0597e
INADA Naoki <songofacandy@gmail.com>
2015-10-31 01:02:58 +0900

    Add example output

--- example/example.go
+++ example/example.go
@@ -26,3 +26,8 @@ func main() {
 	wg.Wait()
 	fmt.Println(stopwatch.Show())
 }
+
+// Output:
+// name	count	avg	total
+// aaa /Users/inada-n/go1.5/src/github.com/methane/stopwatch/example/example.go:16	100	25.500379ms	2.550037924s
+// bbb /Users/inada-n/go1.5/src/github.com/methane/stopwatch/example/example.go:18	100	12.799502ms	1.279950208s
